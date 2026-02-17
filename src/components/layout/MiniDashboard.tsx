903ded800058c65e6634221fb4f3235bcfc0237f
Felizb <felantezb@gmail.com>
2026-02-17 16:15:13 +0300

    added dashboard components

---
+++ src/components/layout/MiniDashboard.tsx
@@ -0,0 +1,13 @@
+import StatsGrid from "./StatsGrid";
+
+function MiniDashboard() {
+  return (
+    <div className="space-y-6">
+
+        {/* Stats Grid */}
+        <StatsGrid />
+     </div>
+  );
+}
+
+export default MiniDashboard;
